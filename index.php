053ed73ee925d24a85fcfedbdb311b83e606a8c3
dembasy <dembasy1227@gmail.com>
2018-07-31 15:42:32 +0200

    done

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// tableau ou on va stocker les différents départements
+$region = array(
+  '59' => 'Nord',
+  '62' => 'Pas de Calais',
+  '02'=> 'Aisne',
+  '60' => 'Oise',
+  '80' => 'Somme');
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title> exercice 1</title>
+  </head>
+  <body>
+  </body>
+</html>
